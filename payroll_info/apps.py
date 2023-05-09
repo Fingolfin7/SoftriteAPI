bedb07a90fd7eed25a0a336ac3a87e364a699bb1
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-05-09 12:41:38 +0200

    move scheduler to apps.py

--- payroll_info/apps.py
+++ payroll_info/apps.py
@@ -1,4 +1,8 @@
 from django.apps import AppConfig
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.conf import settings
+import pytz # to make the datetime.now() timezone aware and get rid of the warning message from 'PytzUsageWarning'
+from datetime import datetime
 
 
 class PayrollInfoConfig(AppConfig):
@@ -7,3 +11,9 @@ class PayrollInfoConfig(AppConfig):
 
     def ready(self):
         import payroll_info.signals
+        from payroll_info.views import update_rbz_rate
+        scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
+        tz = pytz.timezone(settings.TIME_ZONE)
+        scheduler.add_job(update_rbz_rate, 'interval', hours=2, id='update_rbz_rate_job',
+                          next_run_time=tz.localize(datetime.now()))
+        scheduler.start()
