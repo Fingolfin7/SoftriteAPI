61e88809e03d355d06677edb3db70419be79f7eb
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-05-10 08:57:34 +0200

    added misfire grace time for the scheduler

--- payroll_info/apps.py
+++ payroll_info/apps.py
@@ -19,6 +19,6 @@ class PayrollInfoConfig(AppConfig):
 
         # call the update_rbz_rate function at 08:30, 08:45, 09:00, 11:00, 13:00, 15:00, and 16:30 everyday
         scheduler.add_job(update_rbz_rate, 'cron', hour='8-9,11,13,15,16', minute='30,45,0', id='update_rbz_rate_job',
-                          next_run_time=tz.localize(datetime.now()))
+                          misfire_grace_time=60, next_run_time=tz.localize(datetime.now()))
 
         scheduler.start()
