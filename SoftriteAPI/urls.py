efcf27e5405ef87bcccd44b5caa2f374b8605f9c
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-06-26 12:23:19 +0200

    final fix?

--- SoftriteAPI/urls.py
+++ SoftriteAPI/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
@@ -36,4 +35,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
