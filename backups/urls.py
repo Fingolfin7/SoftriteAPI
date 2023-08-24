4c115909b0126b41ef40edac50f8ba1377e14f97
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-08-24 08:44:22 +0200

    Update urls.py

--- backups/urls.py
+++ backups/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('company_list/<int:company_id>', views.CompanyBackupListView.as_view(), name='company_list'),
     path('backup_detail/<int:pk>', views.BackupDetailView.as_view(), name='backup_details'),
     path('file_browser/', views.file_browser_view, name='file_browser'),
-    path('file_browser/<str:path>/', views.file_browser_view, name='file_browser'),
+    path('file_browser/<path:path>/', views.file_browser_view, name='file_browser'),
 ]
