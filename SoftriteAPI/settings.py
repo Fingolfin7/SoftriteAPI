15955ef5e528675832d7d6333b1486d5255b6728
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-10-12 12:16:43 +0200

    Force environment variable update

--- SoftriteAPI/settings.py
+++ SoftriteAPI/settings.py
@@ -16,7 +16,7 @@ import os
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(overwrite=True)  # force the environment variables to be read from the .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
