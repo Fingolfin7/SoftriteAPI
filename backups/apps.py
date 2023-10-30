0556052c3ac4d8c07fd4483f3435dad4e032e176
Henry <63872314+Fingolfin7@users.noreply.github.com>
2023-10-30 09:47:58 +0200

    Removed the cleanup_media_folder() cause we found out why sometimes there are unreferenced images in the profile pic folder
    Why? It's because django_cleanup fails to delete the old images when the file is being held by another process.

--- backups/apps.py
+++ backups/apps.py
@@ -11,6 +11,13 @@ from backups.utils import cleanup_incomplete_uploads, remove_empty_folders
 
 logger = logging.getLogger(__name__)
 
+def clean_function():
+    """container function to run one or more functions from the utils"""
+    cleanup_incomplete_uploads()
+    backups_path = os.path.join(MEDIA_ROOT, 'backups')
+    remove_empty_folders(backups_path, False)
+    # make sure to set remove root to false. It would suck to delete the backups dir by mistake!
+
 
 class BackupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -18,39 +25,6 @@ class BackupsConfig(AppConfig):
 
     def ready(self):
         from users.models import Profile
-
-        def cleanup_media_folder():
-            """
-            I noticed that sometimes when a user uploads a gif as a profile pic, the old profile pic is not deleted.
-            This function will delete all unreferenced images in the profile_pics folder. (its only temporary while I
-            figure out why the old profile pic is not deleted)
-            """
-            referenced_images = [os.path.normpath(os.path.join(MEDIA_ROOT, profile.image.name)) for profile in
-                                 Profile.objects.all()]
-
-            media_folder = os.path.join(MEDIA_ROOT, 'profile_pics')  # Adjust the folder path as needed
-            all_images = [os.path.normpath(os.path.join(media_folder, filename)) for filename in
-                          os.listdir(media_folder)]
-
-            log_text_path = f"{os.sep}".join(os.path.normpath(media_folder).split(os.sep)[-2:])
-
-            # get the set difference to find unreferenced images
-            unreferenced_images = set(all_images) - set(referenced_images)
-
-            logger.info(f"Unreferenced Images in {log_text_path}: {len(unreferenced_images)}")
-            logger.info(f"All Images in {log_text_path}:  {len(all_images)}")
-
-            for image_path in unreferenced_images:
-                logger.info(f"Deleting unreferenced image: {image_path}")
-                os.remove(image_path)
-
-        def clean_function():
-            """container function to run one or more functions from the utils"""
-            cleanup_media_folder()
-            cleanup_incomplete_uploads()
-            backups_path = os.path.join(MEDIA_ROOT, 'backups')
-            remove_empty_folders(backups_path, False)
-            # make sure to set remove root to false. It would suck to delete the backups dir by mistake!
 
         scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
         tz = pytz.timezone(settings.TIME_ZONE)
